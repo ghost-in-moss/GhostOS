f815ce8a0c8b3e1db47af5f57bba7c68026f0eb5
thirdgerb <thirdgerb@gmail.com>
2024-08-04 00:58:33 +0800

    dev: add cache

---
+++ ghostiss/contracts/cache.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from abc import ABCMeta, abstractmethod
+
+
+class Cache(metaclass=ABCMeta):
+    """
+    通用的 cache 封装.
+    """
+
+    @abstractmethod
+    def lock(self, key: str, overdue: int = 0) -> bool:
+        pass
+
+    @abstractmethod
+    def unlock(self, key: str) -> bool:
+        pass
+
+    @abstractmethod
+    def set(self, key: str, val: str, exp: int = 0) -> bool:
+        pass
+
+    @abstractmethod
+    def get(self, key: str) -> str | None:
+        pass
+
+    @abstractmethod
+    def expire(self, key: str, exp: int) -> bool:
+        pass
+
+    @abstractmethod
+    def set_member(self, key: str, member: str, value: str) -> bool:
+        pass
+
+    @abstractmethod
+    def get_member(self, key: str, member: str) -> str | None:
+        pass
+
+    def remove_member(self, key: str, *member: str) -> int:
+        pass
+
+    @abstractmethod
+    def remove(self, *keys: str) -> int:
+        pass
