5d93ecdd268d4e9984cbd973999bb46fe80a4c34
zhuming <thirdgerb@gmail.com>
2024-09-17 20:18:43 +0800

    refact: remove overdue files

--- ghostos/core/moss_p1/__init__.py
+++
@@ -1,4 +0,0 @@
-from ghostos.core.moss_p1.moss import MOSS, System, TestMOSSProvider, BasicPythonMOSSImpl
-from ghostos.core.moss_p1.modules import Modules, BasicModules, BasicModulesProvider
-from ghostos.core.moss_p1.context import PyContext, Imported, Variable
-from ghostos.core.moss_p1.reflect import *
--- ghostos/framework/moss/__init__.py
+++
@@ -1,2 +0,0 @@
-from ghostos.framework.moss.action import MOSSAction, MOSSArgument, DEFAULT_MOSS_FUNCTIONAL_TOKEN
-from ghostos.framework.moss.runner import MossRunner, MOSSRunnerTestSuite, MOSSRunnerTestResult
