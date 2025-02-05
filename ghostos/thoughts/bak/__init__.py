4e0cfc4faeb547e018d8279931fb9c47895e75ce
zhuming <brightred@163.com>
2025-02-05 11:11:18 +0800

    dev: remove test period overdued Thought. tears

--- ghostos/thoughts/bak/__init__.py
+++
@@ -1,6 +0,0 @@
-# abstracts
-from ghostos.thoughts.bak.basic import LLMThoughtDriver
-from ghostos.thoughts.bak.moss_thought import BasicMossThoughtDriver
-from ghostos.thoughts.bak.magic_moss_thought import MagicMossThought
-# implementations
-from ghostos.thoughts.bak.chat import ChatThought
